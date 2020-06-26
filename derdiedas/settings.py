526504d9bb98ee79d42371fde228a932fc7542cd
schneejs <13682559+schneejs@users.noreply.github.com>
2020-06-27 01:48:27 +0600

    Documenting of API module added

--- derdiedas/settings.py
+++ derdiedas/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'lesson.apps.LessonConfig',
     'card.apps.CardConfig',
     'userprofile.apps.UserprofileConfig',
